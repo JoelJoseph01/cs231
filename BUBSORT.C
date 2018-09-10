85562cd717b3c842a7cc98b4475e2977b5d55ebd
ceccs17d29 <43024731+ceccs17d29@users.noreply.github.com>
2018-09-10 09:22:34 +0530

    Update BUBSORT.C

--- BUBSORT.C
+++ BUBSORT.C
@@ -1,23 +1,24 @@
 #include<conio.h>
 #include<stdio.h>
 void main()
-{ int i,j,a[10],n,temp;
-  clrscr();
-  printf("Enter no of elements:");
-  scanf("%d",&n);
-  printf("Enter elements:");
-  for(i=0;i<n;i++)
+{  int i,j,a[10],n,temp;
+   clrscr();
+   printf("Enter no of elements:");
+   scanf("%d",&n);
+   printf("Enter elements:");
+   for(i=0;i<n;i++)
 	scanf("%d",&a[i]);
-  for(i=0;i<n;i++)
+   for(i=0;i<n;i++)
 	{  for(j=0;j<n;j++)
 		{ if (a[j]>a[j+1])
 			{  temp=a[j];
 			   a[j]=a[j+1];
-			   a[j+1]=temp;			}
+			   a[j+1]=temp;			
+			}
 		}
 	}
-  printf("Sorted elements:");
-  for(i=0;i<n;i++)
+   printf("Sorted elements:");
+   for(i=0;i<n;i++)
 	printf("%d",a[i]);
-  getch();
+   getch();
 }
