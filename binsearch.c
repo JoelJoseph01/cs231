827327dfccb61a28dbd83ddf9a2ff393c949e3e8
ceccs17d29 <43024731+ceccs17d29@users.noreply.github.com>
2018-09-18 14:52:52 +0530

    Update binsearch.c

--- binsearch.c
+++ binsearch.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 void main()
-{ int f,l,n,mid,a[10],i,s;
-  printf("Enter no of elements:");
-  scanf("%d",&n);
-  printf("Enter elements:");
-  for(i=0;i<n;i++)
+{  int f,l,n,mid,a[10],i,s;
+   printf("Enter no of elements:");
+   scanf("%d",&n);
+   printf("Enter elements:");
+   for(i=0;i<n;i++)
 	scanf("%d",&a[i]);
-  printf("Enter element to search:");
-  scanf("%d",&s);
-  f=0;
-  l=n-1;
-  mid=(f+l)/2;
-  while(f<=l)
+   printf("Enter element to search:");
+   scanf("%d",&s);
+   f=0;
+   l=n-1;
+   mid=(f+l)/2;
+   while(f<=l)
 	{ if(a[mid]<s)
 		f=mid+1;
           else if(a[mid]==s)
@@ -23,7 +23,7 @@ void main()
           	mid=(f+l)/2;
 	}
 
-  if (f>l)
+   if (f>l)
       printf("Not found!");
 }
 	
