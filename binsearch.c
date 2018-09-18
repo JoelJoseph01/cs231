90b37d5412d77b5fd621aa4ec4bb55a44c1081c5
ceccs17d29 <43024731+ceccs17d29@users.noreply.github.com>
2018-09-18 15:43:54 +0530

    Add files via upload

---
+++ binsearch.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void main()
+{ int f,l,n,mid,a[10],i,s,temp,j;
+  printf("Enter no of elements:");
+  scanf("%d",&n);
+  printf("Enter elements:");
+  for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+  printf("Enter element to search:");
+  scanf("%d",&s);
+  for(i=0;i<n;i++)	
+	{ for(j=0;j<n;j++)
+		{ if(a[i]<a[j])
+			{ temp=a[i];
+			  a[i]=a[j];
+			  a[j]=temp;
+			}
+		}
+	}
+  for(i=0;i<n;i++)
+	printf("%d	\t",a[i]);
+  f=0;
+  l=n-1;
+  mid=(f+l)/2;
+  while(f<=l)
+	{ if(a[mid]<s)
+		f=mid+1;
+          else if(a[mid]==s)
+		{  printf("Element found:%d",mid+1);
+              	   break;
+	        }
+          else 
+		l=mid-1;
+          mid=(f+l)/2;
+	}
+
+  if (f>l)
+      printf("Not found!");
+}
+	
+		
