3156133e53bbf2be8d18d25398ff2ed04e7bc910
ceccs17d29 <43024731+ceccs17d29@users.noreply.github.com>
2018-11-26 17:52:13 +0530

    Update dequell.c

--- dequell.c
+++ dequell.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct node 
 {
 	int data;
@@ -10,7 +9,7 @@ int deleteAll()
 {
 	struct node *ptr = HEADER, *prevPtr;
 	while (ptr != NULL) 
-   {
+        {
 		prevPtr = ptr;
 		ptr = ptr->next;
 		free(prevPtr);
@@ -22,7 +21,7 @@ struct node *newNode()
 {
 	struct node *newptr = malloc(sizeof(struct node));
 	if (newptr == NULL) 
-   {
+        {
 		printf("Memory overflow");
 		deleteAll();
 		exit(0);
@@ -36,7 +35,7 @@ void enqueueRear()
   scanf("%d", &data);
 	struct node *newptr = newNode(), *currentNode = HEADER;
 	while (currentNode->next != NULL)
-   {
+        {
 		currentNode = currentNode->next;
 	}
 	newptr->next = currentNode->next;
@@ -57,43 +56,51 @@ void dequeueRear()
 {
 	struct node *currentNode = HEADER->next, *previousNode=HEADER;
 	if (currentNode == NULL) 
-   {
+        {
 		printf("Empty linked list.\nDeletion failed.\n");
 	} else 
-   {
+        {
 		while (currentNode->next != NULL)
-       {
+        {
 			previousNode = currentNode;
 			currentNode = currentNode->next;
-		}
+			
+		
 		previousNode->next = NULL;
 		free (currentNode);
 	}
 }
-void dequeueFront() {
+void dequeueFront() 
+{
 	struct node *currentNode = HEADER->next;
-	if (currentNode == NULL) {
+	if (currentNode == NULL) 
+	{
 		printf("Empty linked list.\nDeletion failed.\n");
-	} else {
+	} else 
+	{
 		HEADER->next = currentNode->next;
 		free(currentNode);
 	}
 }
-void printQueue() {
+void printQueue() 
+{
 	struct node *currentNode = HEADER->next;
 	printf("\nLinked List\nHEADER->");
-	while (currentNode != NULL) {
+	while (currentNode != NULL) 
+	{
 		printf("%d->", currentNode->data);
 		currentNode = currentNode->next;
 	}
 	printf("NULL\n");
 }
-int main() {
+int main() 
+{
 	char choice;
 	HEADER = malloc(sizeof(struct node));
 	HEADER->data = 0;
 	HEADER->next = NULL;
-	do {
+	do 
+	{
 		printf("\n\tQueue using array\n");
         printf("1.Enqueue Front\n");
         printf("2.Enqueue Rear\n");
@@ -103,7 +110,8 @@ int main() {
         printf("6.Exit\n");
         printf("Enter your choice : ");
 		scanf(" %c", &choice);
-		switch(choice) {
+		switch(choice) 
+		{
 			case '1':
 				enqueueFront();
 				break;
