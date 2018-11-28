5f8b0dc240b642b90fa09ee6b231347cda4ab7e1
ceccs17d29 <43024731+ceccs17d29@users.noreply.github.com>
2018-11-28 06:23:49 +0530

    Update circularquerr.c

--- circularquerr.c
+++ circularquerr.c
@@ -4,63 +4,85 @@
 #define size 10
 int queue[size], front, rear;
 
-void enqueue() {
+void enqueue() 
+{
 	int data;
-  if ((front == rear+1) || (front==0 && rear == size-1)) {
+  if ((front == rear+1) || (front==0 && rear == size-1)) 
+  {
     printf("Queue overflow\n");
-  } else {
+  } else 
+  {
 		printf("Enter data to be entered : ");
 		scanf("%d", &data);
-    if (rear == -1) {
+    if (rear == -1) 
+    {
       rear = front = 0;
-    } else {
+    } else 
+    {
       rear = (rear+1)%size;
     }
     queue[rear] = data;
   }
 }
-void dequeue() {
-  if (front == -1) {
+void dequeue() 
+{
+  if (front == -1) 
+  {
     printf("Queue underflow\n");
-  } else {
-    if (front == rear) {
+  } else 
+  {
+    if (front == rear) 
+    {
       front = rear = -1;
-    } else {
+    } else 
+    {
       front = (front+1)%size;
     }
   }
 }
-void printQueue() {
+void printQueue() 
+{
   int i=0;
-  if (front<=rear) {
-    for (i=0; i<front; i++) {
+  if (front<=rear) 
+  {
+    for (i=0; i<front; i++) 
+    {
       printf("-:");
     }
-    for (;i<=rear; i++) {
+    for (;i<=rear; i++) 
+    {
       printf("%d:", queue[i]);
     }
-    for (;i<size; i++) {
+    for (;i<size; i++) 
+    {
       printf("-:");
     }
-  } else {
-    for (i=0; i<=rear; i++) {
+  } else 
+  {
+    for (i=0; i<=rear; i++) 
+    {
       printf("%d:", queue[i]);
     }
-    for (;i<front; i++) {
+    for (;i<front; i++) 
+    {
       printf("-:");
     }
-    for (;i<size;i++) {
+    for (;i<size;i++) 
+    {
       printf("%d:", queue[i]);
     }
   }
 }
-int main() {
+int main() 
+{
 	char choice;
 	front = rear = -1;
-	do {
+	do 
+	{
 		printf("\n\tQueue using array\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\nEnter your choice : ");
 		scanf(" %c", &choice);
-		switch(choice) {
+		switch(choice) 
+		{
 			case '1':
 				enqueue();
 				break;
