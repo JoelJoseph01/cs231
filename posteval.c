53d32355f8f8a9a619b6ac9c6f45870edeb4a8d2
ceccs17d29 <43024731+ceccs17d29@users.noreply.github.com>
2018-11-28 06:33:34 +0530

    Update posteval.c

--- posteval.c
+++ posteval.c
@@ -2,15 +2,20 @@
      Postfix evaluation */
 #include <stdio.h>
 #include <stdlib.h>
-int power(int base, int exponent) {
-  if (exponent==1) {
+int power(int base, int exponent) 
+{
+  if (exponent==1) 
+  {
     return 1;
-  } else {
+  } else 
+  {
     return base*power(base, exponent-1);
   }
 }
-int evaluvate(int operand1, int operator, int operand2) {
-  switch(operator) {
+int evaluvate(int operand1, int operator, int operand2) 
+{
+  switch(operator) 
+  {
     case '+':
       return (operand1+operand2);
     case '-':
@@ -26,22 +31,27 @@ int evaluvate(int operand1, int operator, int operand2) {
       exit(0);
   }
 }
-int isOperand(char symbol) {
-  if ((symbol>=48 && symbol<=57)) {
+int isOperand(char symbol) 
+{
+  if ((symbol>=48 && symbol<=57)) 
+  {
     return 1;
   }
   return 0;
 }
-int main() {
+int main() 
+{
   char postfix[20];
   int stack[100];
   int i, top=-1, x, y;
   printf("Enter the postfix expression : ");
   scanf(" %s", postfix);
-  for (i=0; postfix[i] != '\0'; i++) {
+  for (i=0; postfix[i] != '\0'; i++) 
+  {
     if (isOperand(postfix[i])) {
       stack[++top] = postfix[i]-48;
-    } else {
+    } else 
+    {
       y = stack[top--];
       x = stack[top];
       stack[top] = evaluvate(x, postfix[i], y);
